e30593390365bb91b7f3726603f0480f8b1fcdab
Mahmoud Bakr <mbakr6821@gmail.com>
2021-09-12 11:33:04 +0200

    init

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  include CurrentUserConcern
+   def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      token = encode_token({ user_id: @user.id })
+      render json: { data: @user, token: token, status: :created }
+    else
+      render json: { message: 'User not found! please try again', status: :unauthorized }
+    end
+  end
+
+  def logged_in
+    if check_logged_in
+      render json: { data: logged_in_user, logged_in: true }
+    else
+      render json: { message: 'Please login first', logged_in: false }
+    end
+  end 
+end
